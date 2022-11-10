d1ad68136b054f0e24030bddf610c4ae872f5165
Valerie Liu <vliu200v@gmail.com>
2022-11-10 18:23:51 -0500

    we running both actuators without the ultrasound

--- All_test/All_test.ino
+++ All_test/All_test.ino
@@ -1,48 +1,65 @@
 #include <Servo.h>
-Servo myservo;
+Servo servox;
+Servo servoy;
 
-const int trigPin = 2;
-const int echoPin = 3;
+//const int trigPin = 2;
+//const int echoPin = 3;
 
 float duration, distance;
 int posx=0;
 int posy=0;
 
 void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+//  pinMode(trigPin, OUTPUT);
+//  pinMode(echoPin, INPUT);
   Serial.begin(9600);
 
-  myservo.attach(5);
+  servox.attach(5);
+  servoy.attach(3);
+  
+  for (posx = 180; posx >=0; posx--) {
+      servoy.write(posx);
+      servox.write(posx);              // tell servo to go to position in variable 'pos'
+//      ultrasound();
+      delay(30);
+    }
 }
 
-void ultrasound() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration*.0343)/2;
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  delay(100);
-}
+//void ultrasound() {
+//  digitalWrite(trigPin, LOW);
+//  delayMicroseconds(2);
+//  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(10);
+//  digitalWrite(trigPin, LOW);
+//
+//  duration = pulseIn(echoPin, HIGH);
+//  distance = (duration*.0343)/2;
+//  Serial.print("Distance: ");
+//  Serial.println(distance);
+//  delay(100);
+//}
 
 void loop() {
-  for(posy=0; posy<=179; posy++){
+  for(posy=0; posy<=180; posy+=30){
     for (posx = 0; posx <= 180; posx += 1) { // goes from 0 degrees to 180 degrees
       // in steps of 1 degree
-      myservo.write(posx);              // tell servo to go to position in variable 'pos'
-      ultrasound();
-//      delay(30);                       // waits 15ms for the servo to reach the position
+      servox.write(posx);              // tell servo to go to position in variable 'pos'
+//      ultrasound();
+      delay(30);                       // waits 15ms for the servo to reach the position
     }
-    posy++;
+    servoy.write(posy);
+    posy+=30;
     for (posx = 180; posx >=0; posx--) {
-      myservo.write(posx);              // tell servo to go to position in variable 'pos'
-      ultrasound();
-//      delay(30);
+      servox.write(posx);              // tell servo to go to position in variable 'pos'
+//      ultrasound();
+      delay(30);
     }
+    servoy.write(posy);
+    delay(30);
   }
+  for (posy = 180; posy >=0; posy--) {
+      servoy.write(posy);              // tell servo to go to position in variable 'pos'
+//      ultrasound();
+      delay(30);
+    }
 }
