f386689f631bd56f9c26e617b76271ee4481fcfc
Sabraj Bhathal <S2bhatha@uwaterloo.ca>
2022-11-10 17:08:43 -0500

    actuator code

---
+++ Actuator_test/actuator_test/actuator_test.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int val;
+
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  myservo.write(35);
+  delay(1500);
+}
